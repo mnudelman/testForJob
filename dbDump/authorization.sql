da28f0989c2163fcb465a98ec9b758b2d0878396
mnudelman <mnudelman@yandex.ru>
2015-12-01 09:55:26 +0500

    2015-12-01T09:55

---
+++ dbDump/authorization.sql
@@ -0,0 +1,128 @@
+-- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: authorization
+-- ------------------------------------------------------
+-- Server version	5.5.46-0ubuntu0.14.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sid` varchar(30) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `begtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `endtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `passwordsave` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sessions`
+--
+
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userprofile`
+--
+
+DROP TABLE IF EXISTS `userprofile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userprofile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) DEFAULT NULL,
+  `surname` varchar(40) DEFAULT '',
+  `name` varchar(40) DEFAULT '',
+  `patronymic` varchar(40) DEFAULT '',
+  `filePhoto` varchar(100) DEFAULT '',
+  `tel` varchar(15) DEFAULT '',
+  `email` varchar(40) DEFAULT '',
+  `sex` char(1) DEFAULT 'm',
+  `birthday` date DEFAULT '2001-01-01',
+  `info` varchar(200) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userprofile`
+--
+
+LOCK TABLES `userprofile` WRITE;
+/*!40000 ALTER TABLE `userprofile` DISABLE KEYS */;
+INSERT INTO `userprofile` VALUES (1,1,'ÐÑƒÐ´ÐµÐ»ÑŒÐ¼Ð°Ð½','ÐœÐ¸Ñ…Ð°Ð¸Ð»','Ð›ÐµÐ¹Ð·ÐµÑ€Ð¾Ð²Ð¸Ñ‡','12345678.jpg','','mnudelman@yandex.ru','m','1954-04-13','Bla...........Bla'),(2,2,'','','','','','','m','2001-01-01',''),(3,3,'','','','','','','m','2001-01-01','');
+/*!40000 ALTER TABLE `userprofile` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) DEFAULT NULL,
+  `password` char(32) DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'12345678','9afb1927c262f4448a75cd71d6edf1c5'),(2,'23456789','9afb1927c262f4448a75cd71d6edf1c5'),(3,'michael_','9afb1927c262f4448a75cd71d6edf1c5');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER  insert_user AFTER INSERT ON users FOR EACH ROW INSERT INTO userprofile (userid) VALUES (new.userId) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-01  1:22:25
---
+++ dbDump/dbAuthorithationScheme.sql
@@ -0,0 +1,47 @@
+-- Создание схемы БД authorization
+-- --------------------------------------
+-- CREATE DATABASE IF NOT EXISTS authorization ;
+-- --------------------------------------
+-- users - список пользователей
+--drop table users ;
+CREATE TABLE IF NOT EXISTS users (
+  userid   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  login    VARCHAR(20) UNIQUE,
+  password CHAR(32)
+)DEFAULT CHARSET=utf8 ;
+-- --------------------------------------
+-- userprofile - Профиль пользователя
+--drop table userprofile ;
+CREATE TABLE IF NOT EXISTS userprofile (
+  id         INTEGER NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+  userid     INTEGER
+             REFERENCES users (userid)
+             ON DELETE CASCADE,
+  surname    VARCHAR(40) DEFAULT '',
+  name       VARCHAR(40) DEFAULT '',
+  patronymic VARCHAR(40) DEFAULT '',
+  filePhoto   VARCHAR(100) DEFAULT '', -- файл с фотографией
+  tel        VARCHAR(15) DEFAULT '',
+  email      VARCHAR(40) DEFAULT '',
+  sex        CHAR(1)           DEFAULT 'm',
+  birthday   DATE DEFAULT '2001-01-01',
+  info       VARCHAR(200) DEFAULT '',
+  CHECK (sex IN ('m', 'w'))
+) DEFAULT CHARSET=utf8;
+-- --------------------------------------
+-- строка в userprofile появляется вместе с users
+CREATE TRIGGER  insert_user AFTER INSERT ON users
+FOR EACH ROW
+  INSERT INTO userprofile (userid) VALUES (new.userId);
+-- --------------------------------------
+-- sessions - сессии пользователей
+drop table sessions ;
+CREATE TABLE IF NOT EXISTS sessions (
+id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+sid VARCHAR (30) ,
+userid  INTEGER
+       REFERENCES users (userid),
+begtime TIMESTAMP,
+endtime TIMESTAMP,
+passwordsave INTEGER default  0
+)DEFAULT CHARSET=utf8 ;
